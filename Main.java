d7f3ab14189729ac0b34cd720d7a5c2e03af4632
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-09 13:15:24 +0000

    The program will ask the user for 10 numbers, it will then ask the user to input  a number to look for which it will then tell the user what line it is on

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will ask for 10 numbers from the user and will find any number in the array based on the user input
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,23 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    int arnum = 10;
+    int[] num = new int[arnum];
+    System.out.print("Please enter 10 integers " );
+    for(int i = 0; i < num.length; i++){
+      
+      num[i] = input.nextInt();
+    }
+    System.out.println("Please enter a number to find");
+    int findnum = input.nextInt();
+    for(int i = 0; i < num.length; i++)
+    if(findnum == num[i]){
+      System.out.println("The number is on line " + (i + 1));
+    }
+
+
+
   }
 }
